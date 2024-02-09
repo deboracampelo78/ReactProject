010aed4b9dda9ad8edb99586246dbe568b2e2e9e
deboracampelo78 <deboracampelo1978@gmail.com>
2024-02-09 20:49:11 -0300

    feat(reactApp) Start new App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Comentario from './components/comentario';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Meu Projeto</h1>
+      <Comentario />
+      <Comentario />
+      <Comentario />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/comentario.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./comentario.css"
+
+//JSX
+const Comentario = () => (
+    <div className="Comentario">
+        <div>João: </div>
+        <div>Olá, tudo bem?</div>
+    </div> 
+);
+
+export default Comentario;
