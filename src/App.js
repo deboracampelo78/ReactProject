7ed57bfa03a45f3fb483c8657e12dfbb5ac4646b
deboracampelo78 <deboracampelo1978@gmail.com>
2024-02-10 22:34:21 -0300

    Criação de eventos e ajustes de estilos

--- src/App.js
+++ src/App.js
@@ -1,19 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 import Comentario from './components/comentario';
+import './App.css';
+import logoSankhya1 from './snakhya1.jpg'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Meu Projeto</h1>
-      <Comentario nome="João" email="joao@gmail.com" data={new Date(2024, 2, 9)}>
-        Olá, tudo bem?
-        </Comentario>
-      <Comentario nome="Maria" email="maria@gmail.com" data={new Date(2024, 2, 11)}>
-        Sim, aqui está tudo ótimo
-        </Comentario>
-    </div>
-  );
+class App extends Component {
+  state = {
+    comentarios: [],
+    novoComentario: {
+      nome: '',
+      email: '',
+      mensagem: '',
+      selecionado: false
+    }
+  }
+
+  adicionarComentario = (event) => {
+    event.preventDefault();
+    const novoComentario = { ...this.state.novoComentario, data: new Date(), selecionado: false };
+    this.setState({
+      comentarios: [...this.state.comentarios, novoComentario],
+      novoComentario: { nome: '', email: '', mensagem: '' }
+    });
+  }
+
+  pegarValoresForm = (event) => {
+    const { name, value } = event.target;
+    this.setState({ novoComentario: { ...this.state.novoComentario, [name]: value } });
+  }
+
+  selecionarComentario = (index) => {
+    const novosComentarios = [...this.state.comentarios];
+    novosComentarios[index].selecionado = !novosComentarios[index].selecionado;
+    this.setState({ comentarios: novosComentarios });
+  }
+
+  removerComentariosSelecionados = () => {
+    const comentariosNaoSelecionados = this.state.comentarios.filter(comentario => !comentario.selecionado);
+    this.setState({ comentarios: comentariosNaoSelecionados });
+  }
+
+  removerComentarioUnico = comentario => {
+    let lista = this.state.comentarios;
+    lista = lista.filter(c => c !== comentario)
+    this.setState({ comentarios: lista })
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+        <h1>Deixe seu comentário</h1>
+        <form method="post" onSubmit={this.adicionarComentario} className="formulario">
+        <img src={logoSankhya1} className='logo-sankhya' alt="Logo Sankhya" />
+          <div>
+            <input
+              type="text"
+              name="nome"
+              value={this.state.novoComentario.nome}
+              onChange={this.pegarValoresForm}
+              placeholder='Digite seu nome'
+              className="input-texto"
+            />
+          </div>
+          <div>
+            <input
+              type="email"
+              name="email"
+              value={this.state.novoComentario.email}
+              onChange={this.pegarValoresForm}
+              placeholder='Digite seu email'
+              className="input-email"
+            />
+          </div>
+          <div>
+            <textarea
+              name="mensagem"
+              rows="4"
+              placeholder='Digite seu comentário'
+              value={this.state.novoComentario.mensagem}
+              onChange={this.pegarValoresForm}
+              className="textarea"
+            ></textarea>
+          </div>
+          <div>
+            <button type="submit" className="botao-enviar">Enviar Comentário</button>
+          </div>
+          <div className="footer">
+            <button type="button" onClick={this.removerComentariosSelecionados} className="botao-remover">Remover Comentários Selecionados</button>
+          </div>
+        </form>
+
+
+        {this.state.comentarios.map((comentario, indice) => (
+          <div key={indice} className="comentario">
+            <Comentario
+              nome={comentario.nome}
+              email={comentario.email}
+              data={comentario.data}
+              onRemove={this.removerComentarioUnico.bind(this, comentario)}
+              onClick={this.selecionarComentario.bind(this, indice)}
+              className="comentario-detalhe">
+              {comentario.mensagem}
+            </Comentario>
+          </div>
+        ))}
+      </div>
+
+    );
+  }
 }
 
 export default App;
--- src/components/comentario.js
+++ src/components/comentario.js
@@ -1,15 +1,30 @@
 import React from "react";
+import { formatRelative } from "date-fns";
+import {ptBR} from 'date-fns/locale'
 import "./comentario.css"
+import imagemUsuario from './user3.png'
 
 //JSX
 //childrem pega os filhos daquele component comentário
 const Comentario = (props) => (
     <div className="Comentario">
-        <h2>{props.nome}</h2>
-        <p>{props.email}</p>
-        <p>{props.children}</p> 
-        <p>{props.data.toString()}</p>
-    </div> 
+        <input
+              type="checkbox"
+              checked={props.selecionado}
+              onClick={props.onClick}
+              className="checkbox"
+            />
+        <img className="avatar" src={imagemUsuario} alt={props.nome} />
+        
+        <div className="conteudo">
+            <h2 className="nome">{props.nome}</h2>
+            <p className="email">{props.email}</p>
+            <p className="mensagem">{props.children}</p>
+            <p className="data">{formatRelative(props.data, new Date(), { locale: ptBR} )}</p>
+            <button type="button" className="button" onClick={props.onRemove}>&times;</button>
+        </div>
+    </div>
+
 );
 
 export default Comentario;
